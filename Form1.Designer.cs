7eaee01cb4374ccd0344c75e5b980351744bf23b
tfhenrique <tiagohenriqueff09@gmail.com>
2023-09-20 09:53:29 +0100

    Frist comit created

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace GIT_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt1 = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(416, 191);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(102, 37);
+            this.bt1.TabIndex = 0;
+            this.bt1.Text = "Gerar";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(233, 160);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 100);
+            this.txt1.TabIndex = 2;
+            this.txt1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.bt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.RichTextBox txt1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GIT_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            Random RandomNumber = new Random();
+            txt1.Text = RandomNumber.Next(1, 11).ToString();
+        }
+    }
+}
